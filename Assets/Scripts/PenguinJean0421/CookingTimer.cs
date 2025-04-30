5adf0ef5edf3bec250b150f2810b2f46ac91ce19
penguinjean0421 <179922506+penguinjean0421@users.noreply.github.com>
2025-04-30 17:45:11 +0900

    손질 타이머 구현

---
+++ Assets/Scripts/PenguinJean0421/CookingTimer.cs
@@ -0,0 +1,126 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class CookingTimer : MonoBehaviour
+{
+    public Renderer colorObj;
+    public Slider timer;
+
+    public string menuName; // 요리 이름
+    public float cookingTime; // 요리 소요 시간
+    public float cuttingTime; // 손질 소요시간
+    public float burned; // 음식이 언제 탔는지 확인할 기준 값
+    float time; // 시간
+
+    /* 화구 타이머 */
+    bool isCooking; // 요리중인지 확인
+    bool isBurned; // 음식이 탔는지 확인
+
+    /* 손질 타이머 */
+    bool isCutting; // 손질중인지 혹인
+    bool isCut; // 음식이 손질 됬는지 확인
+
+    void Start()
+    {
+        timer.value = 0f;
+    }
+    void Update()
+    {
+        // CookingTime(menuName, Time.deltaTime, cookingTime);
+        // CuttingTime(menuName, Time.deltaTime, cookingTime);
+
+        TestCookingTime();
+        TestCuttingTime();
+    }
+
+    #region 함수
+    // 요리 타이머
+    void CookingTime(string menu, float spendTime, float menuTime)
+    {
+        if (!isCooking) return;
+
+        if (!isBurned)
+        {
+            Debug.Log($"{menu} 조리중");
+
+            time += spendTime;
+            Debug.Log(time);
+            timer.value = time / menuTime;
+            if (time >= menuTime * burned)
+            {
+                isBurned = true;
+                Debug.Log("음식이 망했습니다.");
+            }
+        }
+    }
+
+    // 손질 타이머
+    void CuttingTime(string menu, float spendTime, float menuTime)
+    {
+        if (!isCutting) return;
+
+        if (!isCut)
+        {
+            Debug.Log($"{menu} 손질중");
+
+            time += spendTime;
+            Debug.Log(time);
+            timer.value = time / menuTime;
+            if (time >= cuttingTime)
+            {
+                isCut = true;
+                isCutting = false;
+                Debug.Log("손질 완");
+            }
+        }
+    }
+    #endregion
+
+    #region 테스트 코드
+    // 요리 테스트 코드 (Q키 클릭)
+    void TestCookingTime()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (isCooking)
+            {
+                isCooking = false;
+                Time.timeScale = 0f;
+                Debug.Log($"요리 여부  {isCooking}");
+            }
+
+            else
+            {
+                Time.timeScale = 1f;
+                isCooking = true;
+                Debug.Log($"요리 여부  {isCooking}");
+            }
+        }
+        CookingTime(menuName, Time.deltaTime, cookingTime);
+
+        if (isBurned) colorObj.material.color = Color.red;
+    }
+
+    // 손질 테스트 코드 (W키 클릭)
+    void TestCuttingTime()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (isCutting)
+            {
+                isCutting = false;
+                Time.timeScale = 0f;
+                Debug.Log($"손질 여부 : {isCutting}");
+            }
+            else
+            {
+                Time.timeScale = 1f;
+                isCutting = true;
+                Debug.Log($"손질 여부: {isCutting}");
+            }
+        }
+        CuttingTime(menuName, Time.deltaTime, cookingTime);
+
+        if (isCut) colorObj.material.color = Color.green;
+    }
+    #endregion
+}
