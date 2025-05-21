68ef7851a2859ea76d61376ca728f215fd1f3d7b
penguinjean0421 <179922506+penguinjean0421@users.noreply.github.com>
2025-05-21 19:55:12 +0900

    Update Food_Ingredient_Tray.cs

--- Assets/Scripts/DoHwan_Scripts/Food_Ingredient_Tray.cs
+++ Assets/Scripts/DoHwan_Scripts/Food_Ingredient_Tray.cs
@@ -1,9 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 public class Food_Ingredient_Tray : MonoBehaviour
 {
