be6e14c6ad52ed2b6fea57f91e4403b9ef6fbda1
penguinjean0421 <179922506+penguinjean0421@users.noreply.github.com>
2025-05-21 19:07:36 +0900

    Setting 창 구현

---
+++ Assets/Scripts/PenguinJean0421/SettingManager.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class SettingManager : MonoBehaviour
+{
+    public static SettingManager Instance { get; private set; } // 싱글톤
+    SceneLoader sceneLoader; // 씬로더
+
+    bool isPause;
+
+    void Awake()
+    {
+        // 싱글톤 설정
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        sceneLoader = GetComponent<SceneLoader>();
+    }
+
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SettingActive();
+        }
+    }
+
+    // 환경설정 창 실행
+    public void SettingActive()
+    {
+        isPause = !isPause;
+        if (!isPause)  // 창 켜짐
+        {
+            Time.timeScale = 0f;
+            // isPause = true;
+            gameObject.SetActive(isPause);
+        }
+        else // 창 꺼짐
+        {
+            Time.timeScale = 1f;
+            // isPause = false;
+            gameObject.SetActive(isPause);
+        }
+        Debug.Log($"환경설정 창 : {isPause}");
+    }
+}
