72b999548f30af04de569e37f4b9b7c550a888e7
Hsta2614 <dh961231@gmail.com>
2025-06-02 02:57:48 +0900

    타일을 이용한 웨이터 ai생성

---
+++ Assets/Scripts/DoHwan_Scripts/test/Tile.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public Vector2Int coordinates; // 타일 좌표 (x, z)
+    public bool canMove = true; // 이동 가능 여부
+
+    void Start()
+    {
+       
+    }
+    private void Awake()
+    {
+        // 타일의 월드 좌표를 그리드 좌표로 설정 (필요 시 조정)
+        coordinates = new Vector2Int(Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.z));
+        //Debug.Log($"Tile initialized at {coordinates}, canMove: {canMove}, position: {transform.position}");
+    }
+}
