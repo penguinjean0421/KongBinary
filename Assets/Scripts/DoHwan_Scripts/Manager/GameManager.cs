4cc4b3f09f098920549396f96b1012fea22240c1
Hsta2614 <dh961231@gmail.com>
2025-06-14 02:20:24 +0900

    게임매니저 업데이트

--- Assets/Scripts/DoHwan_Scripts/Manager/GameManager.cs
+++ Assets/Scripts/DoHwan_Scripts/Manager/GameManager.cs
@@ -49,7 +49,7 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         playerIndex = 1;
-         ResetGameStart();
+        // ResetGameStart();
         // // ResetGameObj();
         // // ResetTimer(); // 게임 시작 시 타이머 초기화
         // // sales = 0; // 판매액 초기화
@@ -87,6 +87,7 @@ public class GameManager : MonoBehaviour
         if (timeRemaining <= 0)
         {
             timeRemaining = 0;
+            if(timerText!=null)
             GameOver();
         }
 
@@ -99,6 +100,8 @@ public class GameManager : MonoBehaviour
     {
         int minutes = Mathf.FloorToInt(timeRemaining / 60);
         int seconds = Mathf.FloorToInt(timeRemaining % 60);
+        if (timerText == null)
+            return;
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
